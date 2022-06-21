faf61ccf08f3cc89fa6fc847ab372e4eb267c48a
DumboJ <dumbojxx@gmail.com>
2022-06-21 23:56:35 +0800

    自动创建6种类型线程池+手动设置线程池参数

--- chapterone/src/lagou_currency/threadpool/README.md
+++ chapterone/src/lagou_currency/threadpool/README.md
@@ -1,5 +1,24 @@
-## No1. 线程池相比手动创建的优点
-## No1. 认识各类线程池+拒绝策略+任务阻塞队列
-## No1. 线程池相比手动创建的优点
-## No1. 线程池相比手动创建的优点
-## No1. 线程池相比手动创建的优点
+### 1.线程池初识
+### 2.常见线程池类型详解
+
+FixedThreadPool
+SingleThreadExecutor
+CachedThreadPool
+ScheduledThreadPool
+SingleThreadScheduledExecutor
+ForkJoinPool
+### 3.关闭线程池
+shutdown
+shutdownNow
+terminate
+
+### 4.线程池相关参数
+corePoolSize_核心线程数
+maxPoolSize_最大线程数
+keepAliveTime_空闲线程存活时间
+ThreadFactory_线程工厂
+WorkQuene_任务队列
+RejectedExecutionHandler_任务拒绝处理器
+### 5.线程池应用问题相关
+自动创建线程池利弊
+合理设置线程数量
--- chapterone/src/lagou_currency/threadpool/ThreadPoolDemo.java
+++ chapterone/src/lagou_currency/threadpool/ThreadPoolDemo.java
@@ -1,4 +1,32 @@
 package lagou_currency.threadpool;
 
+import java.util.concurrent.*;
+
+import static java.util.concurrent.ThreadPoolExecutor.*;
+
 public class ThreadPoolDemo {
+
+
+    /**
+     * 自动创建几种常见线程池的方法
+     * */
+    public void waysOfCreateAutomatic(){
+        Executors.newFixedThreadPool(10);
+        Executors.newSingleThreadExecutor();
+        Executors.newCachedThreadPool();
+        Executors.newScheduledThreadPool(6);
+        Executors.newSingleThreadScheduledExecutor();
+        /**源码为ForkJoinPool*/
+        Executors.newWorkStealingPool();
+    }
+    public void wayOfCreateByself(){
+        RejectedExecutionHandler AbortPolicy = new ThreadPoolExecutor.AbortPolicy();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 60L, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(30), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread("test-thread-pool%n");
+            }
+        }, AbortPolicy);
+    }
 }
