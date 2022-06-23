663b0a4c71075546bcdc9f266c0b30cce6ddb8e2
DumboJ <dumbojxx@gmail.com>
2022-06-23 23:58:54 +0800

    RetrantLock里读写锁的公平和非公平锁实现策略

--- chapterone/src/lagou_currency/thread_safe/KindsOfLock.java
+++ chapterone/src/lagou_currency/thread_safe/KindsOfLock.java
@@ -29,6 +29,17 @@ public class KindsOfLock {
         new Thread(()->read()).start();
         new Thread(()->write()).start();
         new Thread(()->write()).start();
+        /**
+         * 读写锁在针对不公平锁时候有两类策略：
+         *          1、允许插队          有可能出现读锁在前，中间有写锁时进去队列等待，后来读锁可以插队，导致队列里的写错长时间得不到执行而出现饥饿现象
+         *          2、不允许插队         RetrantLock 使用该策略
+         * ReentrantLock 的读写锁实现策略是：
+         *          写锁在队列前则不允许插队，可以防止读锁插队太多，写锁获取出现“饥饿现象”
+         * */
+        new Thread(()->read(),"read-0").start();
+        new Thread(()->read(),"read-1").start();
+        new Thread(()->write(),"write-0").start();
+        new Thread(()->read(),"read-2").start();
     }
     public static void read(){
         readLock.lock();
@@ -46,7 +57,7 @@ public class KindsOfLock {
         writeLock.lock();
         try {
             System.out.println(Thread.currentThread().getName() + "获取写锁，正在写入");
-            Thread.sleep(500);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }finally {
